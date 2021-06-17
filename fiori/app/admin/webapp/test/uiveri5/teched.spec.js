5fe0fead9b24d371358795f4c80b585aa1d90f85
Sebastian Butzek <sebastian.butzek@sap.com>
2021-06-17 10:58:24 +0200

    add solutions again

--- fiori/app/admin/webapp/test/uiveri5/teched.spec.js
+++ fiori/app/admin/webapp/test/uiveri5/teched.spec.js
@@ -2,8 +2,18 @@ require("./pages/manageBooksPage");
 require("./pages/newBookPage");
 
 describe("teched", function () {
-  it("<describing test scenario>", function () {
-    //When.onThe<>Page.iDoAction();
-    //Then.onThe<>Page.iShouldSeeSomething();
+  let bookTitle = "How to cook Pancakes";
+  it("create a new book", function () {
+    When.onTheManageBooksPage.iClickOnCreateNewBook();
+    When.onTheNewBookPage.iEnterTitle(bookTitle);
+    When.onTheNewBookPage.iSelectGenre();
+    When.onTheNewBookPage.iSelectAuthor();
+    When.onTheNewBookPage.iClickOnSaveButton();
+    Then.onTheNewBookPage.iSeeEditButton();
+  });
+
+  it("should check book is added", function () {
+    When.onTheNewBookPage.iNavigateBack();
+    Then.onTheManageBooksPage.theBookListContains(bookTitle);
   });
 });
